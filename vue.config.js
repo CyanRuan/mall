414094f4a1fb74a22e3e576e870cb8708cf0163f
CyanRuan <nayruan@163.com>
2020-02-06 15:49:36 +0800

    add vue.config.js file && .ediorconfig file

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+modules.exports = {
+    configureWebpack: {
+        reslove: {
+            alais: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'views': '@/views',
+                'network': '@/network',
+            }
+        }
+    }
+}
